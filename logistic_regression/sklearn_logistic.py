eeb01dc0de3bd0231e0afe4662c11559d79f7dcf
fred.feng0326@gmail.com <1126happy>
2019-06-11 14:35:27 +0800

    2019.06.11

--- logistic_regression/sklearn_logistic.py
+++ logistic_regression/sklearn_logistic.py
@@ -13,13 +13,16 @@ X = data.iloc[:, 0:cols - 1]
 y = data.iloc[:, cols - 1:cols]
 X = np.array(X.values)
 y = np.array(y.values)
+# y = y.ravel()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8) # 为了看模型在没有见过数据集上的表现，随机拿出数据集中30%的部分做测试
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5) # 为了看模型在没有见过数据集上的表现，随机拿出数据集中30%的部分做测试
 
 classifier = LogisticRegression()  # 使用类，参数全是默认的
 classifier.fit(X_train, y_train)  # 训练数据来学习，不需要返回值
 
 # 分类测试集，这将返回一个测试结果的数组
 y_pred = classifier.predict(X_train)
-# 计算模型在测试集上的准确性，我的结果为0.9，还不错
-print(accuracy_score(y_test, y_pred))
+print(y_pred)
+# 准确率 0.7875
+print(accuracy_score(y_train, y_pred))
